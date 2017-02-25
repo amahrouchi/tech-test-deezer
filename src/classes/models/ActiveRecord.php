87f16271a4b997cfb7f984d23478bdced5019ae8
Angelo Mahrouchi <am@tradelab.fr>
2017-02-25 18:29:05 +0100

    Handle single ActiveRecord retrieval from DB

--- src/classes/models/ActiveRecord.php
+++ src/classes/models/ActiveRecord.php
@@ -14,19 +14,19 @@ abstract class ActiveRecord
      * The primary key
      * @var array
      */
-    protected $primaryKey = [];
+    protected static $primaryKey = [];
 
     /**
      * The table schema
      * @var array
      */
-    private static $schema;
+    protected static $schema;
 
     /**
      * The data of the current ActiveRecord
      * @var array
      */
-    protected $attributes;
+    protected $attributes = [];
 
     /**
      * The DB connection
@@ -43,35 +43,56 @@ abstract class ActiveRecord
     }
 
     /**
-     * Inits the table schema
-     * @return $this
+     * Gets the object attributes
+     * @return array
      */
-    private function initSchema()
+    public function getAttributes()
     {
-        // Init table schema
-        if (!isset(static::$schema))
+        return $this->attributes;
+    }
+
+    /**
+     * @param int|array $id
+     * @return bool
+     */
+    public function get($id)
+    {
+        if (is_scalar($id))
         {
-            $sql = "DESC $this->tableName";
-            $statement = self::$pdo->query($sql);
-
-            if ($statement === false)
-            {
-                throw new \RuntimeException("Unable to retrieve the schema of the table $this->tableName", 500);
-            }
-
-            $description = $statement->fetchAll(\PDO::FETCH_ASSOC);
-            static::$schema = $description;
-            foreach ($description as $field)
-            {
-                if ($field['Key'] === 'PRI')
-                {
-                    $this->primaryKey[] = $field['Field'];
-                }
-            }
+            $key = reset(static::$primaryKey);
+            $id = [$key => $id];
+        }
+        elseif (!is_array($id) || empty($id))
+        {
+            throw new \RuntimeException('Wrong ID in ActiveRecord class ' . get_called_class(), 500);
         }
 
-        return $this;
+        // Prepare the condition on the primary key
+        $keyCondition = [];
+        $paramToBind = [];
+        foreach ($id as $field => $value)
+        {
+            $keyCondition[] = $field . ' = :' . $field;
+
+            $binding = ':' . $field;
+            $paramToBind[$binding] = $value;
+        }
+        $keyCondition = '(' . implode(' AND ', $keyCondition) . ')';
+
+        // Get the data
+        $sql = "SELECT * FROM $this->tableName WHERE " . $keyCondition . ' LIMIT 1';
+        $statement = self::$pdo->prepare($sql);
+        $statement->execute($paramToBind);
+        $line = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        $this->attributes = isset($line[0]) ? $line[0] : [];
+
+        return !empty($this->attributes);
     }
+
+    //-------------------------------------------//
+    //------------------ STATIC -----------------//
+    //-------------------------------------------//
 
     /**
      * Sets the PDO DB connection
@@ -89,5 +110,55 @@ abstract class ActiveRecord
     public static function getSchema()
     {
         return static::$schema;
+    }
+
+    //--------------------------------------------------------//
+    //------------------ PRIVATE / PROTECTED -----------------//
+    //--------------------------------------------------------//
+
+    /**
+     * Inits the table schema
+     * @return $this
+     */
+    private function initSchema()
+    {
+        // Init table schema
+        if (!isset(static::$schema))
+        {
+            // Check PDO
+            if (empty(self::$pdo))
+            {
+                throw new \RuntimeException('No database connection found to init ActiveRecords', 500);
+            }
+
+            // Check table name
+            if (empty($this->tableName))
+            {
+                throw new \RuntimeException('Undefined table name in class ' . get_called_class(), 500);
+            }
+
+            // Retrieve table schema
+            $sql = "DESC $this->tableName";
+            $statement = self::$pdo->query($sql);
+
+            if ($statement === false)
+            {
+                throw new \RuntimeException("Unable to retrieve the schema of the table $this->tableName", 500);
+            }
+
+            $description = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            static::$schema = $description;
+
+            // Retrieve primary key
+            foreach ($description as $field)
+            {
+                if ($field['Key'] === 'PRI')
+                {
+                    static::$primaryKey[] = $field['Field'];
+                }
+            }
+        }
+
+        return $this;
     }
 }
